e27302320d0aa6af75a10de06c3d3f14c484c9b0
umer khalique <omerkhalique13@gmail.com>
2022-02-17 13:43:03 +0500

    js added

---
+++ app.js
@@ -0,0 +1,50 @@
+let review0 = {
+    photo: 0,
+    name: "Susan Smith",
+    job: "Web Developer",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint!"
+}
+let review1 = {
+    photo: 1,
+    name: "Peter Jones",
+    job: "Intern",
+    review: "Adipisicing elit. Maxime mollitia, molestiae quas vel sint!"
+}
+let review2 = {
+    photo: 2,
+    name: "Anna Johnson",
+    job: "Web Designer",
+    review: "molestiae quas vel sint!"
+}
+let review3 = {
+    photo: 3,
+    name: "Bill Anderson",
+    job: "The Boss",
+    review: "Lorem ipsum dolor. Maxime mollitia, quas vel sint!"
+}
+let review4 = {
+    photo: 4,
+    name: "Mehmood Ghaker",
+    job: "Blockchain Developer",
+    review: "Consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint!"
+}
+let review5 = {
+    photo: 5,
+    name: "Rafay Arshad",
+    job: "Web Developer",
+    review: "Sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint!"
+}
+
+
+
+function prev(){
+    
+}
+
+
+function next(){
+    console.log('hiiii');
+}
+function random(){
+    console.log('hiiiiiiiiiiiiiiiiii');
+}
